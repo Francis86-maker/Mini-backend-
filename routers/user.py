0be5c290a169770f2ba6b336b22baf1c720fdaaa
Francis86-maker <francismukabya@gmail.com>
2026-03-12 06:02:36 +0300

    Implement user creation with email validation
    Add user creation logic with email uniqueness check and password hashing.

---
+++ routers/user.py
@@ -0,0 +1,25 @@
+from .models import UserCreate 
+from fastapi import APIRouter, Depends, HTTPException 
+from ..databasemodels import get_db
+from sqlalchemy import Session
+from . import models
+import bcrypt
+
+router = APIRouter()
+
+@router.post('/user')
+def createuser(request: UserCreate, db: Session=Depends(get_db)):
+  existing_email = db.query(models.User).filter(models.User.email == request.email).first()
+  if existing_email:
+    raise HTTPException(status_code=400, detail="Email already exists")
+  password = request.password.encode("UTF-8")
+  hashedpassword= bcrypt.hashpw(password, bcrypt.gensalt())
+  new_user = models.User(name=request.name, email=request.email, password= hashedpassword)
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
+  return {
+    "name": new_user.name,
+    "email": new_user.email,
+    "message": "user created"
+  }
