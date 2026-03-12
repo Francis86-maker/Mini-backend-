9282e2868a9eba11cb2303a0e0537dbefb8887da
Francis86-maker <francismukabya@gmail.com>
2026-03-12 19:56:11 +0300

    Create deposit.py

---
+++ routers/deposit.py
@@ -0,0 +1,21 @@
+from .models import Deposit
+from fastapi import APIRouter, Depends, HTTPException 
+from ..databasemodels import get_db
+from sqlalchemy import Session
+from . import models 
+
+router = APIRouter()
+
+@router.post('/deposit')
+def deposit(request: Deposit, db: Session=Depends(get_db)):
+  user = db.query(models.User).filter(models.User.id == request.id).first()
+  if not user:
+    raise HTTPException(status_code=404, detail="user not found")
+  if request.amount <= 0:
+    raise HTTPException(status_code=400, detail="amount is too low")
+  user.balance += request.amount
+  db.commit()
+  db.refresh(user)
+  return {
+    "message": "successful"
+  }
